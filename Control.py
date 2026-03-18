464e5a2be69d1fc0e32ab39dade82df76a21402f
keenanlang <klang@aps.anl.gov>
2026-03-18 18:18:26 -0500

    More error handling

--- Control.py
+++ Control.py
@@ -4,8 +4,8 @@ import json
 import requests
 import argparse
 
-from caproto import ChannelType
-from caproto.server import PVGroup, ioc_arg_parser, pvproperty, run, scan_wrapper
+from caproto import ChannelType, SkipWrite
+from caproto.server import PVGroup, ioc_arg_parser, pvproperty, run, scan_wrapper, get_pv_pair_wrapper
 
 #########################
 #   IOC Implementation  #
@@ -26,74 +26,82 @@ class ControlIOC(PVGroup):
 			
 	async def start_stop(self, instance, value):
 		if value < 0 or value > 1:
-			return instance.value
-
+			raise SkipWrite
+			
 		if value == 0 and instance.value == 1:
 			pass
-			
-			# # Do Stop
-			# try:
-				# response = requests.put(self.url + "/filewriter/config/mode", json.dumps({"value" : "stop"}), timeout=5)
-				
-				# if response.status_code != 200:
-					# await self.DetectorState_RBV.write("Error")
-					# await self.StatusMessage_RBV.write("Non-success response from API: %s" % response.status_code)
-					# return instance.value
-				
-			# except:
-				# await self.DetectorState_RBV.write("Error")
-				# await self.StatusMessage_RBV.write("Timeout error")
-				# return instance.value
 			
 		elif value == 1 and instance.value == 0:
 			# Do Start
 			try:
-				response = requests.put(self.url + "/filewriter/config/mode", json.dumps({"value" : "start"}), timeout=5)
+				response = await requests.put(self.url + "/filewriter/config/ntrains", json.dumps({"value" : self.numimages.readback.value}), timeout=5)
 				
 				if response.status_code != 200:
-					await self.DetectorState_RBV.write("Error")
-					await self.StatusMessage_RBV.write("Non-success response from API: %s" % response.status_code)
-					return instance.value
+					raise Exception("Non-success response from API: %s" % response.status_code)
+			
+				response = await requests.put(self.url + "/filewriter/config/output_dir", json.dumps({"value" : self.OutputDir.value}), timeout=5)
+				
+				if response.status_code != 200:
+					raise Exception("Non-success response from API: %s" % response.status_code)
+				
+				response = await requests.put(self.url + "/filewriter/config/mode", json.dumps({"value" : "start"}), timeout=5)
+				
+				if response.status_code != 200:
+					raise Exception("Non-success response from API: %s" % response.status_code)
 				
 				await self.AcquireBusy.write("Acquiring")
 				await self.DetectorState_RBV.write("Acquire")
 					
-			except:
+			except Exception as e:
 				await self.DetectorState_RBV.write("Error")
-				await self.StatusMessage_RBV.write("Timeout error")
-				return instance.value
+				await self.StatusMessage_RBV.write(str(e)[0:255])
+				raise SkipWrite
 				
 		else:
-			return instance.value
+			raise SkipWrite
 
 		await self.StatusMessage_RBV.write("")
 		return value
 
 
-	# Start/Stop
+	# PV's
 	Acquire = pvproperty(value=0, put=start_stop)
 	AcquireBusy = pvproperty(dtype=ChannelType.ENUM, enum_strings=("Done", "Acquiring"))
 	DetectorState_RBV = pvproperty(dtype=ChannelType.ENUM, enum_strings=("Idle", "Acquire", "Readout", "Correct", "Saving", "Aborting", "Error"))
-	StatusMessage_RBV = pvproperty(value="")
+	StatusMessage_RBV = pvproperty(value="", dtype=ChannelType.STRING, max_length=255)
 	status_check = pvproperty(value="")
+	OutputDir = pvproperty(value="", dtype=ChannelType.STRING, max_length=255)
 	
+	pair_pvs = get_pv_pair_wrapper(setpoint_suffix='',
+						           readback_suffix='_RBV')
+						
+	numimages = pair_pvs(name="NumImages", value=0)
+	
+	@numimages.setpoint.putter
+	async def numimages(obj, instance, value):
+		await obj.readback.write(value)
+						
+			
 	@status_check.scan(period=1.0)
 	async def status_check(self, instance, async_io):
-		if self.DetectorState_RBV.value == 0:
+		if self.DetectorState_RBV.value != 1:
 			return
 		
-		response = requests.get(self.url + "/filewriter/config/mode", timeout=1)
-		
-		if response.json()["value"] == "start":
-			return
+		try:
+			response = await requests.get(self.url + "/filewriter/config/mode", timeout=1)
 			
-		response = requests.get(self.url + "/filewriter/status/waiting_ntrains", timeout=1)
-		
-		if response.json()["value"] == 0:
-			await self.Acquire.write(0)
-			await self.AcquireBusy.write("Done")
-			await self.DetectorState_RBV.write("Idle")
-			await self.StatusMessage_RBV.write("")
+			if response.json()["value"] == "start":
+				return
+				
+			response = await requests.get(self.url + "/filewriter/status/waiting_ntrains", timeout=1)
+			
+			if response.json()["value"] == 0:
+				await self.Acquire.write(0)
+				await self.AcquireBusy.write("Done")
+				await self.DetectorState_RBV.write("Idle")
+				await self.StatusMessage_RBV.write("")
+		except:
+			pass
 
 			
 			
