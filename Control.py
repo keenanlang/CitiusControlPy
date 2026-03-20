6226636cd55b90855d2276059f6d0a0f3a9fdca6
keenanlang <klang@aps.anl.gov>
2026-03-20 14:19:40 -0500

    Fixing some datatype issues with caproto

--- Control.py
+++ Control.py
@@ -34,17 +34,25 @@ class ControlIOC(PVGroup):
 		elif value == 1 and instance.value == 0:
 			# Do Start
 			try:
-				response = await requests.put(self.url + "/filewriter/config/ntrains", json.dumps({"value" : self.numimages.readback.value}), timeout=5)
+				if self.numimages.readback.value <= 0:
+					raise Exception("NumImages must be > 0")
+
+				metadata, values = await self.OutputDir.read(ChannelType.STRING)
+				
+				if len(values[0]) == 0:
+					raise Exception("Must define OutputDir")
+					
+				response = requests.put(self.url + "/filewriter/config/ntrains", json.dumps({"value" : int(self.numimages.readback.value)}), timeout=5)
 				
 				if response.status_code != 200:
 					raise Exception("Non-success response from API: %s" % response.status_code)
 			
-				response = await requests.put(self.url + "/filewriter/config/output_dir", json.dumps({"value" : self.OutputDir.value}), timeout=5)
+				response = requests.put(self.url + "/filewriter/config/output_dir", json.dumps({"value" : str(values[0])}), timeout=5)
 				
 				if response.status_code != 200:
 					raise Exception("Non-success response from API: %s" % response.status_code)
 				
-				response = await requests.put(self.url + "/filewriter/config/mode", json.dumps({"value" : "start"}), timeout=5)
+				response = requests.put(self.url + "/filewriter/config/mode", json.dumps({"value" : "start"}), timeout=5)
 				
 				if response.status_code != 200:
 					raise Exception("Non-success response from API: %s" % response.status_code)
@@ -76,31 +84,49 @@ class ControlIOC(PVGroup):
 						           readback_suffix='_RBV')
 						
 	numimages = pair_pvs(name="NumImages", value=0)
+	#imagemode = pair_pvs(name="ImageMode", dtype=ChannelType.ENUM, enum_strings=("Normal", "Burst"))
 	
 	@numimages.setpoint.putter
 	async def numimages(obj, instance, value):
 		await obj.readback.write(value)
 						
+	# @imagemode.setpoint.putter
+	# async def imagemode(obj, instance, value):				
+		# try:
+			# response = requests.put(obj.parent.url + "/detector/config/imaging_mode", json.dumps({"value" : value.lower()}), timeout=5)
+		
+			# if response.status_code != 200:
+				# raise SkipWrite("Response Code: " + str(response.status_code))
+			
+		# except Exception as e:
+			# print(obj.parent.prefix, "ImageMode", e)
+			# raise SkipWrite 
+		
+		# response = requests.get(obj.parent.url + "/detector/config/imaging_mode")
+			
+		# await obj.readback.write(response.json()["value"].capitalize())
+		
 			
 	@status_check.scan(period=1.0)
 	async def status_check(self, instance, async_io):
-		if self.DetectorState_RBV.value != 1:
+		if str(self.DetectorState_RBV.value) != "Acquire":
 			return
 		
 		try:
-			response = await requests.get(self.url + "/filewriter/config/mode", timeout=1)
+			response = requests.get(self.url + "/filewriter/config/mode", timeout=1)
 			
-			if response.json()["value"] == "start":
+			if str(response.json()["value"]) == "start":
 				return
 				
-			response = await requests.get(self.url + "/filewriter/status/waiting_ntrains", timeout=1)
+			response = requests.get(self.url + "/filewriter/status/waiting_ntrains", timeout=1)
 			
-			if response.json()["value"] == 0:
+			if int(response.json()["value"]) == 0:
 				await self.Acquire.write(0)
 				await self.AcquireBusy.write("Done")
 				await self.DetectorState_RBV.write("Idle")
 				await self.StatusMessage_RBV.write("")
-		except:
+		except Exception as e:
+			print(e)
 			pass
 
 			
@@ -112,32 +138,30 @@ class ControlIOC(PVGroup):
 if __name__ == '__main__':
 	
 	# Setup commandline args
-	parser = argparse.ArgumentParser(prog='Control.py', usage='%(prog)s [OPTIONS]', formatter_class=argparse.RawTextHelpFormatter, exit_on_error=True)
-	parser.add_argument("--ip", metavar="IP", dest="ip", action="store", type=str, default="192.168.1.33", help="""\
+	myparser = argparse.ArgumentParser(prog='Control.py', usage='%(prog)s [OPTIONS]', formatter_class=argparse.RawTextHelpFormatter, exit_on_error=True)
+	myparser.add_argument("--ip", metavar="IP", dest="ip", action="store", type=str, default="192.168.1.33", help="""\
 IP Address for the Citius Web API, default is 192.168.1.33
 """)
 
-	parser.add_argument("--port", metavar="PORT", dest="port", action="store", type=int, default=30303, help="""\
+	myparser.add_argument("--port", metavar="PORT", dest="port", action="store", type=int, default=30303, help="""\
 Network port for the Citius Web API, default is 30303
 """)
 
-	parser.add_argument("--prefix", metavar="PREFIX", dest="prefix", action="store", type=str, default="citius:", help="""\
+	myparser.add_argument("--prefix", metavar="PREFIX", dest="prefix", action="store", type=str, default="citius:", help="""\
 IOC Prefix for PVs, default is "citius:". PVs will also have "cam1:" prepended.
 """)
 
-	args = parser.parse_args()
+	args = myparser.parse_args()
 	
-	# Create IOC
-	ioc_options, run_options = ioc_arg_parser(
-		default_prefix=args.prefix + 'cam1:',
-		desc='IOC')
-	
-	citius_control = ControlIOC(**ioc_options)
+	# Create IOC	
+	citius_control = ControlIOC(prefix=args.prefix+"cam1:")
 	citius_control.url = 'http://%s:%d' % (args.ip, args.port)
-
+	
 	try:
 		citius_control.initialize()
-		run(citius_control.pvdb, **run_options)
+		print(citius_control.prefix + " Connected to Detector")
+		
+		run(citius_control.pvdb)
 	except Exception as e:
 		print("Error starting IOC:", e)
 	
